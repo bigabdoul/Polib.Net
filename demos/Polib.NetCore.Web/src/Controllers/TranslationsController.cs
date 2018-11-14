df261b26383ff16ca7e53fb2a55cbcfabf7bde8d
bigabdoul <kababdoul@hotmail.com>
2018-11-14 19:18:19 +0000

    add catalog merge support

--- demos/Polib.NetCore.Web/src/Controllers/TranslationsController.cs
+++ demos/Polib.NetCore.Web/src/Controllers/TranslationsController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Polib.Net;
+using Polib.Net.IO;
 using Polib.Net.Synchronization;
 using Polib.NetCore.Web.Models;
+using System;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Polib.NetCore.Web.Controllers
 {
@@ -16,14 +20,15 @@ namespace Polib.NetCore.Web.Controllers
 
         public IActionResult Index() => View(Manager.Catalogs);
 
-        public IActionResult Catalog(string id)
+        public IActionResult Catalog(string id, int? newentries)
         {
             if (!FindCatalog(id, out var catalog))
             {
                 ViewBag.Message = T("Catalog id '{0}' not found!", id);
                 return NotFound();
             }
-            
+
+            ViewBag.NewEntries = newentries;
             return View(catalog);
         }
 
@@ -66,6 +71,54 @@ namespace Polib.NetCore.Web.Controllers
                 }
             }
             return Json(false);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult MergeCatalog(string id)
+        {
+            try
+            {
+                if (FindCatalog(id, out var current))
+                {
+                    var count = 0;
+                    var files = Request.Form.Files.Where(f => f.Length > 0L).ToArray();
+
+                    if (files.Length > 0)
+                    {
+                        foreach (var file in files)
+                        {
+                            using (var fstream = file.OpenReadStream())
+                            {
+                                using (var tr = new StreamReader(fstream, System.Text.Encoding.UTF8))
+                                {
+                                    var catalog = PoFileReader.Read(tr, Domain, enforceCultureInfo: false);
+                                    count += current.MergeWith(catalog);
+                                }
+                            }
+                        }
+                    }
+
+                    // show the catalog to update
+                    return RedirectToAction(nameof(Catalog), new { id, newentries = count });
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+                return Error();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Refresh()
+        {
+            Manager.ResetTranslations();
+            await Manager.LoadTranslationsAsync(Domain);
+            return RedirectToAction(nameof(Index));
         }
 
         #region helpers
